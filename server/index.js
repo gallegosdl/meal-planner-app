05d1805619839734f215ac47ebfe3c5cd0a4a5a5
Darrell Gallegos <gallegosdl1975@gmail.com>
2025-05-21 13:10:08 -0700

    added render URL

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://your-frontend-url.onrender.com'
+    ? 'https://meal-planner-app-frontend-s4ga.onrender.com'
     : 'http://localhost:3000'
 }));
 app.use(express.json());
