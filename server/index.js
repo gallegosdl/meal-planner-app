59547ea300461f5cb9f1e7fbb86e4091ba65a930
Darrell Gallegos <gallegosdl1975@gmail.com>
2025-05-21 13:36:45 -0700

    second attempt to deploy

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://meal-planner-app-frontend-s4ga.onrender.com'
+    ? 'https://meal-planner-frontend-uzh6.onrender.com'
     : 'http://localhost:3000'
 }));
 app.use(express.json());
