2f2aa66fe2a09fda9e7822318289761d7f210e31
Darrell Gallegos <gallegosdl1975@gmail.com>
2025-05-31 09:12:51 -0700

    npm install react-hot-toast and change for toast

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import MealPlannerForm from './components/MealPlannerForm';
 
 function App() {
   return (
     <div className="App">
       <MealPlannerForm />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#2A3142',
+            color: '#fff',
+          },
+          success: {
+            iconTheme: {
+              primary: '#4ade80',
+              secondary: '#2A3142',
+            },
+          },
+          error: {
+            iconTheme: {
+              primary: '#ef4444',
+              secondary: '#2A3142',
+            },
+          },
+        }}
+      />
     </div>
   );
 }
