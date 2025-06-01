da81a85768cc5273b54236ce110715bd6d3dcfb6
Darrell Gallegos <gallegosdl1975@gmail.com>
2025-06-01 14:34:34 -0700

    Attempt to use DOCKER

--- server/services/instacartScraper.js
+++ server/services/instacartScraper.js
@@ -27,6 +27,7 @@ class InstacartScraper {
       console.log('Scraper: Starting browser initialization');
       this.browser = await puppeteer.launch({
         headless: 'new',
+        executablePath: '/usr/bin/google-chrome-stable',
         args: [
           '--no-sandbox',
           '--disable-setuid-sandbox',
