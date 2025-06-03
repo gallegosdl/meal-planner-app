38b45a3d9ea8bdfb79a10c37ad4cf218b6757250
Darrell Gallegos <gallegosdl1975@gmail.com>
2025-06-02 18:26:15 -0700

    config file

--- server/.puppeteerrc.cjs
+++ server/.puppeteerrc.cjs
@@ -4,6 +4,5 @@ const {join} = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  cacheDirectory: '/opt/render/.cache/puppeteer',
-  executablePath: '/usr/bin/google-chrome'
+  cacheDirectory: '/opt/render/.cache/puppeteer'
 }; 
