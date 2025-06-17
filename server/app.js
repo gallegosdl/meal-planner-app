3f76b8cbdf960578a2892ff89d66c3b32acf3761
Darrell Gallegos <gallegosdl1975@gmail.com>
2025-06-16 17:02:26 -0700

    needed changes for routing

--- server/app.js
+++ server/app.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const app = express();
+
 const pantryRoutes = require('./routes/pantry');
 
+app.use(express.json()); 
+
 // Register routes
-app.use('/api/pantry', pantryRoutes); 
+app.use('/api/pantry', pantryRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
